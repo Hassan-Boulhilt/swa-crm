3ca0d5aa99153e58bdca52e2bcdde98f352f5d73
hboulhilt <hboulhilt@gmail.com>
2021-02-25 21:02:21 +0100

    update admin.py to include models and create home html

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Lead
 
 
 def home_page(request):
-    return render(request, "leads/home.html", )
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/home.html", context)
