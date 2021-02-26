66419be6ea06cbfdb6a140546ceeb5d94e1ef118
hassan <hboulhilt@gmail.com>
2021-02-26 17:10:00 +0100

    update create, detail, list tempates

--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,9 @@
+from leads.views import lead_create, lead_detail, lead_list
 from django.urls import path
-from .views import home_page
+
 app_name = "leads"
 urlpatterns = [
-    path('', home_page)
+    path('', lead_list, name='lead_list'),
+    path('detail/<id>', lead_detail, name='lead_detail'),
+    path('create/', lead_create, name='lead_create'),
 ]
