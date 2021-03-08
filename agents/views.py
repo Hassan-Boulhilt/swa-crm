86e202110868bd6f09cf15ecb182d2a2cbc2f180
hassan <hboulhilt@gmail.com>
2021-03-09 00:15:55 +0100

    add agent form creation and send email invitation to him

--- agents/views.py
+++ agents/views.py
@@ -1,5 +1,8 @@
-from django.views import generic
+import random
 
+
+from django.core.mail import send_mail
+from django.views import generic
 from django.shortcuts import render, reverse
 from leads.models import Agent
 from .forms import AgentModelForm
@@ -15,9 +18,24 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         return reverse('agents:agent-list')
 
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organisation = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organisor = False
+        user.set_password(f"{random.randint(0, 1000000)}")
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organisation=self.request.user.userprofile,
+        )
+        send_mail(
+            subject="You are invited to be an agent",
+            message="You were added as an agent by swa-crm, Please come login to start working",
+            from_email="test@test.com",
+            recipient_list=[user.email]
+        )
+
+        # agent.organisation = self.request.user.userprofile
+        # agent.save()
 
         return super(AgentCreateView, self).form_valid(form)
 
