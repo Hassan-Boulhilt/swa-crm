ae036f067df66976e5e1f2eca70dc8cc553cb254
hassan <hboulhilt@gmail.com>
2021-03-16 00:39:31 +0100

    nc

--- swacrm/settings.py
+++ swacrm/settings.py
@@ -28,7 +28,7 @@ if READ_DOT_ENV_FILE:
 
 
 # False if not in os.environ
-DEBUG = env('DEBUG')
+DEBUG = True
 
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
